9361ded5384b4a6f544fdfffc1339d81b1583717
Chiara Bigarella <chiara.bigarella1@gmail.com>
2018-06-14 09:48:36 +0200

    scripts: script to update robot and machine list

---
+++ counter_robots/config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of COUNTER-Robots.
+# Copyright (C) 2018 CERN.
+#
+# COUNTER-Robots is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Counter-robots configurations."""
+
+config = dict(
+    ROBOTS_URL='https://raw.githubusercontent.com/CDLUC3/Make-Data-Count/master/user-agents/lists/robot.txt',
+    MACHINE_URL='https://raw.githubusercontent.com/CDLUC3/Make-Data-Count/master/user-agents/lists/machine.txt',
+    ROBOTS_FILE='counter_robots/data/robot.txt',
+    VERSION_FILE='counter_robots/data/machine.txt'
+)
