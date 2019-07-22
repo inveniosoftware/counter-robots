039afc4860e2861b24667c7706f516d62bf6dcb4
Jorik van Kemenade <jorik.van.kemenade@cern.ch>
2019-07-22 11:54:11 +0200

    scripts: diff COUNTER-Robots and Make-Data-Count lists
    Make-Data-Count uses a non up-to-date version of the COUNTER-Robots
    list. However, the differentiation between robots and machines is nice.
    To make sure that we have the latest list, but with differentiation we
    diff the COUNTER-Robots robots.txt with the machines.txt and
    unclassified.txt from the Make-Data-Count repository. If the resulting
    classification contains errors, than this must be fixed by opening a PR
    at either source repository.
    
    Testruns on Python 2.7 had the following error during pip isntall:
    
        ERROR: pytest-cov 2.7.1 has requirement pytest>=3.6,
        but you'll have pytest 3.3.2 which is incompatible.
    
    This caused problems during the test. Bumping the pytest version
    resolves this.

--- scripts/update-lists.py
+++ scripts/update-lists.py
@@ -6,7 +6,8 @@
 # COUNTER-Robots is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""Small script to update the list of robots and machines.
+"""
+Small script to update the list of robots and machines.
 
 Usage:
 
@@ -26,35 +27,75 @@ except ImportError:
     from urllib import urlopen
 
 
-files = [
-    ('machine.txt',
-     'https://raw.githubusercontent.com/CDLUC3/Make-Data-Count/'
-     'master/user-agents/lists/machine.txt'),
-    ('robot.txt',
-     'https://raw.githubusercontent.com/CDLUC3/Make-Data-Count/'
-     'master/user-agents/lists/robot.txt'),
-]
+files = {
+    'machine.txt':
+        'https://raw.githubusercontent.com/CDLUC3/Make-Data-Count/'
+        'master/user-agents/lists/machine.txt',
+    'robot.txt':
+        'https://raw.githubusercontent.com/atmire/COUNTER-Robots/'
+        'master/generated/COUNTER_Robots_list.txt',
+    'unclassified.txt':
+        'https://raw.githubusercontent.com/CDLUC3/Make-Data-Count/'
+        'master/user-agents/lists/unclassified.txt'
+
+}
+
+def _create_set(filename):
+    """Create a set containing all lines in the file"""
+    with open(_get_package_path(filename), 'r') as fp:
+        return set(fp.readlines())
 
 def _get_package_path(filename):
     """Retrieve path of a file in this Python package."""
     return join(dirname(__file__), '../counter_robots/data/', filename)
 
 
-def update_file(url, filename):
+def _update_file(url, filename):
     """Update the content of a single file."""
-    resp = urlopen(url)
-    if resp.code != 200:
+    response = urlopen(url)
+    if response.code != 200:
         raise Exception('GET {} failed.'.format(url))
-    with open(_get_package_path(filename), 'w') as fp:
-        for l in resp:
-            if not l.startswith(b'#'):
-                fp.write(l.decode('utf8'))
+
+    user_agents = [agent.decode('utf-8') 
+                   for agent in response if not agent.startswith(b'w')]
+
+    with open(_get_package_path(filename), 'wb') as fp:
+        for agent in user_agents:
+            fp.write(agent.encode('utf-8'))
+
     print('Updated {}'.format(filename))
+
+
+def _filter_machines_from_robots(files):
+    """
+    The Counter-Robots repository keeps a list of all user-agents that are
+    regarded robots/spyders. The Make-Data-Count repository uses manually
+    curated lists to seperate machine user-agents from robot user-agents.
+
+    Unfortunately Make-Data-Count is on a less frequent update schedule than the
+    COUNTER-Robots repository. To make sure that we have the latest user agents
+    available to use we fetch the latest Counter-Robots data and diff it with
+    the latest machine/unclassified user-agents known from Make-Data-Count.
+    """
+    machine_set = _create_set('machine.txt')
+    unclassified_set = _create_set('unclassified.txt')
+    robot_set = _create_set('robot.txt')
+
+    robot_set = robot_set.difference(machine_set, unclassified_set)
+    robot_set = sorted(robot_set, key=str.lower)
+
+    with open(_get_package_path('robot.txt'), 'w') as fp:
+        fp.writelines(robot_set)
+
+    print('Filtered robot.txt')
+
 
 def main():
     """Update the files containing the list of robots and machines."""
-    for filename, url in files:
-        update_file(url, filename)
+    for filename, url in files.items():
+        _update_file(url, filename)
+
+    _filter_machines_from_robots(files)
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ tests_require = [
     'pydocstyle>=1.0.0',
     'pytest-cov>=1.8.0',
     'pytest-pep8>=1.0.6',
-    'pytest>=2.8.0',
+    'pytest>=3.6',
 ]
 
 extras_require = {
