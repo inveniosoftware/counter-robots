e17155d9a33be44def14fbf9c3eedf76ac2f0e81
Saksham <sakshamarora1001@gmail.com>
2025-02-04 15:35:16 +0100

    setup: Migrate from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,81 +1,13 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of COUNTER-Robots.
-# Copyright (C) 2018 CERN.
+# Copyright (C) 2025 CERN.
 #
 # COUNTER-Robots is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
 """Library for COUNTER-compliant detection of machines and robots."""
 
-import os
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-readme = open('README.rst').read()
-history = open('CHANGES.rst').read()
-
-tests_require = [
-    'pytest-invenio>=1.4.0',
-]
-
-extras_require = {
-    'docs': [
-        'Sphinx>=1.5.1',
-    ],
-    'tests': tests_require,
-}
-
-extras_require['all'] = []
-for reqs in extras_require.values():
-    extras_require['all'].extend(reqs)
-
-setup_requires = [
-    'pytest-runner>=2.6.2',
-]
-
-install_requires = []
-
-packages = find_packages()
-
-
-# Get the version string. Cannot be done with import!
-g = {}
-with open(os.path.join('counter_robots', 'version.py'), 'rt') as fp:
-    exec(fp.read(), g)
-    version = g['__version__']
-
-setup(
-    name='counter-robots',
-    version=version,
-    description=__doc__,
-    long_description=readme + '\n\n' + history,
-    keywords='COUNTER user-agent robot detection',
-    license='MIT',
-    author='CERN',
-    author_email='info@inveniosoftware.org',
-    url='https://github.com/inveniosoftware/counter-robots',
-    packages=packages,
-    zip_safe=False,
-    include_package_data=True,
-    platforms='any',
-    extras_require=extras_require,
-    install_requires=install_requires,
-    setup_requires=setup_requires,
-    tests_require=tests_require,
-    classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Development Status :: 5 - Production/Stable',
-    ],
-)
+setup()
